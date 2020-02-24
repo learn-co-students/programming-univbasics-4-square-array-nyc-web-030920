eb55bec00982874b20360e55d48e403f9970fbc9
delightful-source-3050 <joshmrallen@gmail.com>
2020-02-24 16:30:13 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
   # your code here
+  
+  index = 0
+  squares = []
+  
+  while index < array.length do
+    squares[index] = (array[index] * array[index])
+    index+=1
+  end
+  
+  p squares
+  
 end
+
