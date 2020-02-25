f145551a6c2abd1d138e8108084a247cef70e662
Duncan Uruchima <duruchima1@gmail.com>
2020-02-25 16:40:17 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
+
 def square_array(array)
-  # your code here
+    counter=0
+    new_array=[]
+    while array[counter] do
+        n=array[counter]
+        new_array.push(n*n)
+        counter+=1
+      end
+return new_array
 end
